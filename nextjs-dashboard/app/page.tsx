2104a616ac3bf3a06315b69e47f2da3e78dea286
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2026-01-23 22:43:26 +0000

    update

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -18,11 +18,7 @@ export default function Page() {
           className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
         />*/}
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Handcraft Haven</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Handcraft Haven</strong><br></br>A home for artisans and makers who pour heart, skill, and creativity into every piece they create. Discover unique, handcrafted products and connect directly with the talented creators behind them. Every item here tells a story. Yours is just beginning!
           </p>
           <Link
             href="/login"
