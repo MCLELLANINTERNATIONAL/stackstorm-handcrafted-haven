d572bfd459af930ae65b3a148992765eb6be8cdc
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2026-02-17 23:12:16 +0000

    add ui/dashboard.button

---
+++ nextjs-dashboard/app/ui/dashboard/button.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button({ className = '', ...props }: ButtonProps) {
+  return (
+    <button
+      className={`flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 disabled:opacity-50 ${className}`}
+      {...props}
+    />
+  );
+}
